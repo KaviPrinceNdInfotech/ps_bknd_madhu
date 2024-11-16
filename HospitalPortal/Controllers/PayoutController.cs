1ffe0f3e46a6b93abd4f0ec92c5298968b8d0cbe
mmm <139609691+rajputmadhu108@users.noreply.github.com>
2024-11-16 11:54:59 +0530

    akku

--- HospitalPortal/Controllers/PayoutController.cs
+++ HospitalPortal/Controllers/PayoutController.cs
@@ -1748,6 +1748,7 @@ ORDER BY Id";
             Sheet.Cells["M1"].Value = "Phone No";
             int row = 2;
             CRNGenerator crnGenerator = new CRNGenerator();
+
             foreach (DetailsForBank item in employeeDetails)
             {
                 string dvrId = item.UniqueId;
